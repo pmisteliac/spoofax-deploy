7ed81f92ad6cb8d0c3b8bd53f71765fc271de4ae
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2020-07-17 10:24:48 +0200

    Add SPT language

--- gradle/settings.gradle.kts
+++ gradle/settings.gradle.kts
@@ -37,6 +37,10 @@ fun String.includeProject(id: String, dir: String = id, path: String = "../../$t
   includeProject("stratego.compiler.pack")
 }
 
+"spt".run {
+  includeProject("org.metaborg.meta.lang.spt")
+}
+
 "nabl".run {
   includeProject("nabl2.terms")
   includeProject("nabl2.lang")
