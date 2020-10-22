c80682baef6b0c0b25252f1faf3aaecec31eef41
mpsijm <mpsijm@gmail.com>
2020-10-22 09:19:23 +0200

    Replace virtualenv with venv (#32)

--- releng/run.sh
+++ releng/run.sh
@@ -14,16 +14,35 @@ else
   exit 1
 fi
 
-# Ensure that virtualenv is installed and updated
-$PYTHON_CMD -m pip install --quiet --user --upgrade --requirement "$DIR/requirements_virtualenv.txt"
+VENV="$DIR/.venv"
 
-# Create and activate virtualenv
-VENV="$DIR/.virtualenv"
-$PYTHON_CMD -m virtualenv --quiet "$VENV" > /dev/null
-# Since virtualenv is made by idiots, temporarily disable unbound variable checks when activating virtualenv
-set +o nounset
+# Create virtual environment, if it does not exist yet
+if [[ ! -d "$VENV" ]]
+then
+    # On Ubuntu/Debian, Python3 does not automatically come with ensurepip installed
+    # https://bugs.launchpad.net/ubuntu/+source/python3.4/+bug/1290847/+index?comments=all
+    # Note that the issue is marked as "fixed", but it's still broken.
+    if ! $PYTHON_CMD -c 'help("modules")' 2> /dev/null | grep ensurepip
+    then
+        echo "Could not find the 'ensurepip' module, which is required for creating a"
+        echo "Python virtual environment. There are a few options to fix this, for example:"
+        echo "- Install python3-venv using your system's package manager, e.g.:"
+        # This fix comes from comments #63-64 on the Ubuntu bug report
+        echo "    sudo apt install python3-venv"
+        echo "- Create the virtual environment manually without Pip and install Pip inside it:"
+        # This fix comes from comment #58 on the Ubuntu bug report
+        echo "    $PYTHON_CMD -m venv --without-pip \"$VENV\""
+        echo "    curl -s https://bootstrap.pypa.io/get-pip.py | \"$VENV/bin/python\""
+        echo "After executing one of these options, you can re-run this command."
+        exit 1
+    fi
+
+    # This command is silent by itself, but errors (if any) are printed to stdout
+    $PYTHON_CMD -m venv "$VENV"
+fi
+
+# Activate virtual environment
 source "$VENV/bin/activate"
-set -o nounset
 
 # Install requirements
 $PYTHON_CMD -m pip install --quiet --requirement "$DIR/requirements.txt"
